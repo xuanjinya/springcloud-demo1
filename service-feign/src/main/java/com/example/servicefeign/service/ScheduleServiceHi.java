21d471e88e5565047e85e866ee7c84a7f17bfde6
HuangBeiJin <1067998193@qq.com>
2022-06-22 21:04:54 +0800

    断路器（Hystrix）

--- service-feign/src/main/java/com/example/servicefeign/service/ScheduleServiceHi.java
+++ service-feign/src/main/java/com/example/servicefeign/service/ScheduleServiceHi.java
@@ -5,8 +5,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(value = "service-hi")
+@FeignClient(value = "service-hi", fallback = ScheduleServiceHiHystrix.class)
 public interface ScheduleServiceHi {
+
     @RequestMapping(value = "/hi", method = RequestMethod.GET)
     String sayHiFromClientOne(@RequestParam(value = "name") String name);
+
 }
--- service-ribbon/src/main/java/com/example/serviceribbon/service/HelloService.java
+++ service-ribbon/src/main/java/com/example/serviceribbon/service/HelloService.java
@@ -1,5 +1,6 @@
 package com.example.serviceribbon.service;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -10,8 +11,16 @@ public class HelloService {
     @Autowired
     RestTemplate restTemplate;
 
+    /*
+     * 改造HelloService类，在hiService方法上加上@HystrixCommand注解
+     * */
+    @HystrixCommand(fallbackMethod = "hiError")
     public String hiService(String name) {
         return restTemplate.getForObject("http://SERVICE-HI/hi?name=" + name, String.class);
+    }
+
+    public String hiError(String name) {
+        return "hi," + name + ",sorry,error!";
     }
 
 }
