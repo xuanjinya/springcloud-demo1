a75f2f08b00b5ce04ddf14c3224c71af769e900c
HuangBeiJin <1067998193@qq.com>
2022-06-22 17:43:35 +0800

    Feign

---
+++ service-feign/src/main/java/com/example/servicefeign/controller/HiController.java
@@ -0,0 +1,21 @@
+package com.example.servicefeign.controller;
+
+import com.example.servicefeign.service.ScheduleServiceHi;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HiController {
+
+    @Autowired
+    ScheduleServiceHi scheduleServiceHi;
+
+    @RequestMapping(value = "/hi", method = RequestMethod.GET)
+    public String sayHi(@RequestParam String name) {
+        return scheduleServiceHi.sayHiFromClientOne(name);
+    }
+
+}
